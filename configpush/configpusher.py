5a4618fc493f532d194e4f2a874971c5df3fc5d3
manikandarajzma <manikandaraj@protonmail.com>
2019-04-06 18:45:38 -0500

    worked around the enable secret issue by adding priv 15. dealing with scp issues

--- configpush/configpusher.py
+++ configpush/configpusher.py
@@ -1,9 +1,8 @@
 from nornir import InitNornir
 from nornir.plugins.tasks.data import load_yaml
 from nornir.plugins.tasks.text import template_file
-from nornir.plugins.tasks.networking import netmiko_send_config
+from nornir.plugins.tasks.networking import napalm_configure
 from nornir.plugins.functions.text import print_result
-from nornir.core.inventory import ConnectionOptions
 
 def load_data(task):
     data = task.run(
@@ -15,13 +14,12 @@ def load_data(task):
     task.host["networks"] = data.result["networks"]
     r = task.run(task=template_file, template="bgp.j2", path="")
     task.host["template_config"] = r.result
-    task.run(task=netmiko_send_config, configuration=task.host["template_config"])
+    task.run(task=napalm_configure, configuration=task.host["template_config"])
 
 
 
 nr = InitNornir()
-nr.inventory.defaults.connection_options = ConnectionOptions(extras={"secret": "cisco"})
-routers = nr.filter(platform='cisco_ios')
+routers = nr.filter(platform='ios')
 
 r = routers.run(load_data)
 print_result(r)
